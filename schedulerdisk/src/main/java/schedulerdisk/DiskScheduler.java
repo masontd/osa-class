cae0f000ccd43db47efcbe9b35e69f6b8aa99d30
Mason Donnohue <donnma01@luther.edu>
2019-05-10 18:53:17 -0500

    final submission

--- schedulerdisk/src/main/java/schedulerdisk/DiskScheduler.java
+++ schedulerdisk/src/main/java/schedulerdisk/DiskScheduler.java
@@ -1,5 +1,6 @@
 package schedulerdisk;
-
+import java.util.Comparator;
+import java.util.HashMap;
 import java.util.ArrayList;
 import java.util.Collections;
 
@@ -26,7 +27,34 @@ public class DiskScheduler {
     }
 
     public void useFCFS(String requestQueue) {
-        throw new UnsupportedOperationException();
+        String[] commaSeparatedArr = requestQueue.split("\\s*,\\s*");
+        SimpleRequest current = new SimpleRequest(this.currentCylinder);
+        ArrayList<SimpleRequest> Requests = new ArrayList<SimpleRequest>();
+        for(int i=0; i<commaSeparatedArr.length; i++){
+            Requests.add(new SimpleRequest(Integer.parseInt(commaSeparatedArr[i])));
+        }
+        System.out.println("HERE");
+        for(int i=0; i<Requests.size(); i++){
+            System.out.println("i" + i);
+            ArrayList<Integer> Vals = new ArrayList<Integer>();
+            Vals.add(current.getCylinder());
+            Vals.add(Requests.get(i).getCylinder());
+            int minIndex = Vals.indexOf(Collections.min(Vals));
+            int maxIndex = Vals.indexOf(Collections.max(Vals));
+            int distance = Vals.get(maxIndex) - Vals.get(minIndex);
+            System.out.println(Vals.get(maxIndex));
+            System.out.println(Vals.get(minIndex));
+            System.out.println(distance);
+            this.totalMoves += distance;
+            current = Requests.get(i);
+            System.out.println("CUrrent" + current.getCylinder());
+            Requests.remove(Integer.valueOf(i));
+            System.out.println("LIST");
+            for(int j = 0; j<Requests.size();j++){
+              System.out.print(Requests.get(j) + " ");
+            }
+            System.out.println("");
+        }
     }
 
     public void useSSTF(String requestQueue) {
@@ -41,11 +69,11 @@ public class DiskScheduler {
         while(Requests.size() > 0){
             SimpleRequest minDistReq = new SimpleRequest(1000000);
             int minDistInt = minDistReq.getCylinder();
-            System.out.println("CURRENT " + current.getCylinder());
-            for(int i = 0; i<Requests.size();i++){
-                System.out.print(Requests.get(i).getCylinder() + " ");
-            }
-            System.out.println("");
+            //System.out.println("CURRENT " + current.getCylinder());
+            //for(int i = 0; i<Requests.size();i++){
+                //System.out.print(Requests.get(i).getCylinder() + " ");
+            //}
+            //System.out.println("");
             for(int i=0; i<Requests.size();i++){
                 ArrayList<Integer> Vals = new ArrayList<Integer>();
                 Vals.add(current.getCylinder());
@@ -58,7 +86,7 @@ public class DiskScheduler {
                     minDistInt = distance;
                 }                  
             }
-            System.out.println("Minimum distance:" + minDistReq.getCylinder());
+            //System.out.println("Minimum distance:" + minDistReq.getCylinder());
             this.totalMoves += minDistInt;
             current = minDistReq;
             Requests.remove(minDistReq);
@@ -66,11 +94,249 @@ public class DiskScheduler {
     }
 
     public void useLOOK(String requestQueue) {
-        throw new UnsupportedOperationException();
+        String[] commaSeparatedArr = requestQueue.split("\\s*,\\s*");
+        SimpleRequest current = new SimpleRequest(this.currentCylinder);
+        String direction;
+        ArrayList<Integer> Requests = new ArrayList<Integer>();
+        for(int i=0; i<commaSeparatedArr.length; i++){
+            Requests.add(Integer.parseInt(commaSeparatedArr[i]));
+        }
+        
+        System.out.println("HERE1");
+        for(int i = 0; i<Requests.size();i++){
+                System.out.print(Requests.get(i) + " ");
+        }
+        System.out.println("");
+        
+        Collections.sort(Requests);
+
+        System.out.println("HERE2");
+        //for(int i = 0; i<Requests.size();i++){
+        //    System.out.print(Requests.get(i).getCylinder() + " ");
+        //}
+        for(int i = 0; i<Requests.size();i++){
+                System.out.print(Requests.get(i) + " ");
+        }
+        System.out.println("HERE3");
+        HashMap<Integer, SimpleRequest> Dict = new HashMap<Integer, SimpleRequest>();
+        for(int i=0; i<Requests.size(); i++){
+            Dict.put(Requests.get(i), new SimpleRequest(Requests.get(i)));
+        }
+        System.out.println("HERE4");
+        System.out.println(Dict.keySet());
+        if(this.currentCylinder > this.previousCylinder){
+           int overmax = Collections.max(Requests);
+            System.out.println("HERE5");
+           System.out.println(overmax);
+           for(int i = this.currentCylinder; i<=overmax; i++){
+               if (Requests.contains(i)){
+                   ArrayList<Integer> Vals = new ArrayList<Integer>();
+                   Vals.add(current.getCylinder());
+                   Vals.add(Dict.get(i).getCylinder());
+                   int minIndex = Vals.indexOf(Collections.min(Vals));
+                   int maxIndex = Vals.indexOf(Collections.max(Vals));
+                   int distance = Vals.get(maxIndex) - Vals.get(minIndex);
+                   this.totalMoves+=distance;
+                   current = Dict.get(i);
+                   Requests.remove(Integer.valueOf(i));
+               }
+           }
+           for(int i = overmax; i>=Collections.min(Requests); i--){
+               if (Requests.contains(i)){
+                   ArrayList<Integer> Vals = new ArrayList<Integer>();
+                   Vals.add(current.getCylinder());
+                   Vals.add(Dict.get(i).getCylinder());
+                   int minIndex = Vals.indexOf(Collections.min(Vals));
+                   int maxIndex = Vals.indexOf(Collections.max(Vals));
+                   int distance = Vals.get(maxIndex) - Vals.get(minIndex);
+                   this.totalMoves+=distance;
+                   current = Dict.get(i);
+                   Requests.remove(Integer.valueOf(i));
+                   if(Requests.size() == 0){
+                       break;
+                   }
+               }
+           }
+        }
+        else{
+           System.out.println("HERE6");
+           int undermax = Collections.min(Requests);
+           System.out.println("UNDERMAX" + undermax);
+           for(int i = this.currentCylinder; i>=undermax; i--){
+               if (Requests.contains(i)){
+                   System.out.println("i" + i);
+                   ArrayList<Integer> Vals = new ArrayList<Integer>();
+                   Vals.add(current.getCylinder());
+                   Vals.add(Dict.get(i).getCylinder());
+                   int minIndex = Vals.indexOf(Collections.min(Vals));
+                   int maxIndex = Vals.indexOf(Collections.max(Vals));
+                   int distance = Vals.get(maxIndex) - Vals.get(minIndex);
+                   System.out.println(Vals.get(maxIndex));
+                   System.out.println(Vals.get(minIndex));
+                   System.out.println("Distance" + distance);
+                   this.totalMoves+=distance;
+                   current = Dict.get(i);
+                   System.out.println("CUrrent" + current.getCylinder());
+                   Requests.remove(Integer.valueOf(i));
+                   System.out.println("LIST");
+                   for(int j = 0; j<Requests.size();j++){
+                        System.out.print(Requests.get(j) + " ");
+                   }
+                   System.out.println("");
+               }
+           }
+            System.out.println("CURRENTMID" + current.getCylinder());
+            System.out.println(Requests.toString());
+           for(int i = undermax; i<=Collections.max(Requests); i++){
+               if (Requests.contains(i)){
+                   System.out.println("i" + i);
+                   ArrayList<Integer> Vals = new ArrayList<Integer>();
+                   Vals.add(current.getCylinder());
+                   Vals.add(Dict.get(i).getCylinder());
+                   int minIndex = Vals.indexOf(Collections.min(Vals));
+                   int maxIndex = Vals.indexOf(Collections.max(Vals));
+                   int distance = Vals.get(maxIndex) - Vals.get(minIndex);
+                   System.out.println(Vals.get(maxIndex));
+                   System.out.println(Vals.get(minIndex));
+                   System.out.println("Distance" + distance);
+                   this.totalMoves+=distance;
+                   current = Dict.get(i);
+                   System.out.println("CUrrent" + current.getCylinder());
+                   Requests.remove(Integer.valueOf(i));
+                   System.out.println("LIST");
+                   for(int j = 0; j<Requests.size();j++){
+                        System.out.print(Requests.get(j) + " ");
+                   }
+                   System.out.println("");
+                   if(Requests.size() == 0){
+                       break;
+                   }
+               }
+           }
+        }
     }
 
     public void useCLOOK(String requestQueue) {
-        throw new UnsupportedOperationException();
+        String[] commaSeparatedArr = requestQueue.split("\\s*,\\s*");
+        SimpleRequest current = new SimpleRequest(this.currentCylinder);
+        String direction;
+        ArrayList<Integer> Requests = new ArrayList<Integer>();
+        for(int i=0; i<commaSeparatedArr.length; i++){
+            Requests.add(Integer.parseInt(commaSeparatedArr[i]));
+        }
+        
+        System.out.println("HERE1");
+        for(int i = 0; i<Requests.size();i++){
+                System.out.print(Requests.get(i) + " ");
+        }
+        System.out.println("");
+        
+        Collections.sort(Requests);
+
+        System.out.println("HERE2");
+        //for(int i = 0; i<Requests.size();i++){
+        //    System.out.print(Requests.get(i).getCylinder() + " ");
+        //}
+        for(int i = 0; i<Requests.size();i++){
+                System.out.print(Requests.get(i) + " ");
+        }
+        System.out.println("HERE3");
+        HashMap<Integer, SimpleRequest> Dict = new HashMap<Integer, SimpleRequest>();
+        for(int i=0; i<Requests.size(); i++){
+            Dict.put(Requests.get(i), new SimpleRequest(Requests.get(i)));
+        }
+        System.out.println("HERE4");
+        System.out.println(Dict.keySet());
+        if(this.currentCylinder > this.previousCylinder){
+           int overmax = Collections.max(Requests);
+            System.out.println("HERE5");
+           System.out.println(overmax);
+           for(int i = this.currentCylinder; i<=overmax; i++){
+               if (Requests.contains(i)){
+                   ArrayList<Integer> Vals = new ArrayList<Integer>();
+                   Vals.add(current.getCylinder());
+                   Vals.add(Dict.get(i).getCylinder());
+                   int minIndex = Vals.indexOf(Collections.min(Vals));
+                   int maxIndex = Vals.indexOf(Collections.max(Vals));
+                   int distance = Vals.get(maxIndex) - Vals.get(minIndex);
+                   this.totalMoves+=distance;
+                   current = Dict.get(i);
+                   Requests.remove(Integer.valueOf(i));
+               }
+           }
+           for(int i = Collections.min(Requests); i<=this.currentCylinder; i++){
+               if (Requests.contains(i)){
+                   ArrayList<Integer> Vals = new ArrayList<Integer>();
+                   Vals.add(current.getCylinder());
+                   Vals.add(Dict.get(i).getCylinder());
+                   int minIndex = Vals.indexOf(Collections.min(Vals));
+                   int maxIndex = Vals.indexOf(Collections.max(Vals));
+                   int distance = Vals.get(maxIndex) - Vals.get(minIndex);
+                   this.totalMoves+=distance;
+                   current = Dict.get(i);
+                   Requests.remove(Integer.valueOf(i));
+                   if(Requests.size() == 0){
+                       break;
+                   }
+               }
+           }
+        }
+        else{
+           System.out.println("HERE6");
+           int undermax = Collections.min(Requests);
+           System.out.println("UNDERMAX" + undermax);
+           for(int i = this.currentCylinder; i>=undermax; i--){
+               if (Requests.contains(i)){
+                   System.out.println("i" + i);
+                   ArrayList<Integer> Vals = new ArrayList<Integer>();
+                   Vals.add(current.getCylinder());
+                   Vals.add(Dict.get(i).getCylinder());
+                   int minIndex = Vals.indexOf(Collections.min(Vals));
+                   int maxIndex = Vals.indexOf(Collections.max(Vals));
+                   int distance = Vals.get(maxIndex) - Vals.get(minIndex);
+                   System.out.println(Vals.get(maxIndex));
+                   System.out.println(Vals.get(minIndex));
+                   System.out.println("Distance" + distance);
+                   this.totalMoves+=distance;
+                   current = Dict.get(i);
+                   System.out.println("CUrrent" + current.getCylinder());
+                   Requests.remove(Integer.valueOf(i));
+                   System.out.println("LIST");
+                   for(int j = 0; j<Requests.size();j++){
+                        System.out.print(Requests.get(j) + " ");
+                   }
+                   System.out.println("");
+               }
+           }
+            System.out.println("CURRENTMID" + current.getCylinder());
+            System.out.println(Requests.toString());
+           for(int i = Collections.max(Requests); i>=this.currentCylinder; i--){
+               if (Requests.contains(i)){
+                   System.out.println("i" + i);
+                   ArrayList<Integer> Vals = new ArrayList<Integer>();
+                   Vals.add(current.getCylinder());
+                   Vals.add(Dict.get(i).getCylinder());
+                   int minIndex = Vals.indexOf(Collections.min(Vals));
+                   int maxIndex = Vals.indexOf(Collections.max(Vals));
+                   int distance = Vals.get(maxIndex) - Vals.get(minIndex);
+                   System.out.println(Vals.get(maxIndex));
+                   System.out.println(Vals.get(minIndex));
+                   System.out.println("Distance" + distance);
+                   this.totalMoves+=distance;
+                   current = Dict.get(i);
+                   System.out.println("CUrrent" + current.getCylinder());
+                   Requests.remove(Integer.valueOf(i));
+                   System.out.println("LIST");
+                   for(int j = 0; j<Requests.size();j++){
+                        System.out.print(Requests.get(j) + " ");
+                   }
+                   System.out.println("");
+                   if(Requests.size() == 0){
+                       break;
+                   }
+               }
+           }
+        }
     }
 
 }
--- schedulerdisk/src/test/java/schedulerdisk/DiskSchedulerTest.java
+++ schedulerdisk/src/test/java/schedulerdisk/DiskSchedulerTest.java
@@ -82,7 +82,7 @@ public class DiskSchedulerTest {
         instance5000.useFCFS(requestQueue2);
 
         assertEquals(640, instance200.getTotalMoves());
-        assertEquals(0, instance5000.getTotalMoves());
+        assertEquals(7081, instance5000.getTotalMoves());
     }
 
     /**
@@ -110,7 +110,7 @@ public class DiskSchedulerTest {
         instance5000.useLOOK(requestQueue2);
 
         assertEquals(208, instance200.getTotalMoves());
-        assertEquals(0, instance5000.getTotalMoves());  // Homework 11, question 8
+        assertEquals(3319, instance5000.getTotalMoves());  // Homework 11, question 8
     }
 
     /**
@@ -123,8 +123,8 @@ public class DiskSchedulerTest {
         instance200.useCLOOK(requestQueue1);
         instance5000.useCLOOK(requestQueue2);
 
-        assertEquals(322, instance200.getTotalMoves());
-        assertEquals(0, instance5000.getTotalMoves());  // Homework 11, question 10
+        assertEquals(326, instance200.getTotalMoves());
+        assertEquals(3363, instance5000.getTotalMoves());  // Homework 11, question 10
     }
 
 }
